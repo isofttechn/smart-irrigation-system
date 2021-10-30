9af2adf583f7b97e5161ea61fed91fc5a309b54b
isofttechn <isofttechn@gmail.com>
2021-10-30 15:52:57 -0700

    Added New Feature for Gauge Temperature 10-30-2021

--- lib/SensorScreen/charts/chartComponents/TemGauge.dart
+++ lib/SensorScreen/charts/chartComponents/TemGauge.dart
@@ -16,11 +16,32 @@ class TemGauge extends StatelessWidget {
           borderRadius: BorderRadius.circular(5.0),
         ),
         color: kBgColor,
-        child: Container(
-          child: SfRadialGauge(
-              // mainAxisAlignment: MainAxisAlignment.center,
-
-              ),
+        child: Center(
+          child: Container(
+            child: SfRadialGauge(
+                // mainAxisAlignment: MainAxisAlignment.center,
+                axes: <RadialAxis>[
+                  RadialAxis(minimum: 0, maximum: 150, ranges: <GaugeRange>[
+                    GaugeRange(
+                        startValue: 0, endValue: 50, color: Colors.green),
+                    GaugeRange(
+                        startValue: 50, endValue: 100, color: Colors.orange),
+                    GaugeRange(
+                        startValue: 100, endValue: 150, color: Colors.red)
+                  ], pointers: <GaugePointer>[
+                    NeedlePointer(value: 90)
+                  ], annotations: <GaugeAnnotation>[
+                    GaugeAnnotation(
+                        widget: Container(
+                            child: Text('90.0',
+                                style: TextStyle(
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.bold))),
+                        angle: 90,
+                        positionFactor: 0.5)
+                  ])
+                ]),
+          ),
         ),
       ),
     );
