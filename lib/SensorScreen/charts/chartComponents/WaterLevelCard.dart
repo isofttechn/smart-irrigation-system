4a0ada5c95d4e552c60d610e6384fc32a5ecb551
FunkyMan4415 <funkyman@ok.de>
2021-10-31 10:48:21 +0100

    allow to select a previous date with a dropdown

--- lib/SensorScreen/charts/chartComponents/WaterLevelCard.dart
+++ lib/SensorScreen/charts/chartComponents/WaterLevelCard.dart
@@ -1,4 +1,5 @@
 import 'package:api_example_app/SensorScreen/charts/chartComponents/cubit/water_level_cubit.dart';
+import 'package:api_example_app/SensorScreen/charts/model/feed.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:liquid_progress_indicator/liquid_progress_indicator.dart';
@@ -26,44 +27,72 @@ class WaterLevelCard extends StatefulWidget {
 class _WaterLevelCardState extends State<WaterLevelCard> {
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<WaterLevelCubit, WaterLevelState>(
-      builder: (context, state) {
-        if (state.waterLevelResponse == null) {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-        return Container(
-          margin: const EdgeInsets.only(top: 15.0),
-          // width: 500,
-          height: 500,
-          child: LiquidCircularProgressIndicator(
-            value:
-                double.parse(state.waterLevelResponse!.feeds.last.percentage) /
-                    100.0,
-            valueColor: AlwaysStoppedAnimation(
-              double.parse(state.waterLevelResponse!.feeds.last.percentage) <
-                      50.0
-                  ? Colors.red.withOpacity(0.7)
-                  : Color(0xFFd4f1f9),
-            ),
-            backgroundColor: Colors.white,
-            borderColor: Colors.black54,
-            borderWidth: 0.0,
-            direction: Axis.vertical,
-            center: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                    '${double.parse(state.waterLevelResponse!.feeds.last.percentage).toStringAsFixed(2)} %'),
-                Text(
-                    'last Update: ${state.waterLevelResponse!.feeds.last.createdAt}'),
-                Text('last api Call: ${state.lastApiCall} '),
-              ],
-            ),
-          ),
-        );
-      },
+    return Column(
+      children: [
+        BlocBuilder<WaterLevelCubit, WaterLevelState>(
+          buildWhen: (previous, current) =>
+              previous.isLoaded != current.isLoaded,
+          builder: (context, state) {
+            if (state.waterLevelResponse == null) {
+              return const SizedBox.shrink();
+            }
+            final feeds = state.waterLevelResponse!.feeds;
+            return DropdownButton<Feed>(
+              value: context.watch<WaterLevelCubit>().state.selectedFeed,
+              onChanged: (value) =>
+                  context.read<WaterLevelCubit>().updateTank(value!),
+              items: feeds
+                  .map(
+                    (e) => DropdownMenuItem(
+                      value: e,
+                      child: Text(e.createdAt),
+                    ),
+                  )
+                  .toList(),
+            );
+          },
+        ),
+        BlocBuilder<WaterLevelCubit, WaterLevelState>(
+          builder: (context, state) {
+            if (state.waterLevelResponse == null) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            return Container(
+              margin: const EdgeInsets.only(top: 15.0),
+              width: 500,
+              height: 450,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: LiquidLinearProgressIndicator(
+                  //OR USE LiquidCircularProgressIndicator
+                  value: double.parse(state.selectedFeed!.percentage) / 100.0,
+                  valueColor: AlwaysStoppedAnimation(
+                    double.parse(state.selectedFeed!.percentage) < 50.0
+                        ? Colors.red.withOpacity(0.7)
+                        : Color(0xFF3f9ff0),
+                  ),
+                  backgroundColor: Colors.white,
+                  borderColor: Colors.black54,
+                  borderWidth: 0.5,
+                  direction: Axis.vertical,
+                  center: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                          '${double.parse(state.selectedFeed!.percentage).toStringAsFixed(2)} %'),
+                      Text('Last Update: ${state.selectedFeed!.createdAt}'),
+                      Text('Last api Call: ${state.lastApiCall} '),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ],
     );
   }
 }
